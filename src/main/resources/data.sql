b67606f74d49cdc942f3e617d0c7d62a990399b1
Hubert Bullen <Hubert_99_bullen@hotmail.com>
2015-07-13 09:24:37 +0200

    Merge branch 'master' of https://github.com/kvanrobbroeck/vdab-starter
    # Conflicts:
    #	.gitignore
    #	pom.xml

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,20 @@
 
-INSERT INTO film(title, length, summary, genre, director, rating) VALUES('Iron Man', 221, 'Fe male', 'ACTION', 'Feigh Kevin', 5);
-INSERT INTO film(title, length, summary, genre, director, rating) VALUES('Iron Man 2', 211, 'Fe male 2', 'ACTION', 'Feigh Kevin', 5);
-INSERT INTO film(title, length, summary, genre, director, rating) VALUES('Iron Man 3', 225, 'Fe male 3', 'ACTION', 'Feigh Kevin', 5)
+
+INSERT INTO film(title, length, summary, cover_image, genre, director, rating)
+VALUES('Iron Man', 126, 'After being held captive in an Afghan cave, an industrialist creates a unique weaponized suit of armor to fight evil.',
+       LOAD_FILE('/Users/jeansmits/Dropbox/IdeaProjects/Film-Project/src/main/resources/covers/IronMan_dvdcover-744015.jpg'), 'ACTION', 'Jon Favreau', 5);
+
+INSERT INTO film(title, length, summary, genre, director, rating)
+VALUES('Iron Man 2', 124, 'With the world now aware of his identity as Iron Man, Tony Stark must contend with both his declining health and a vengeful mad man with ties to his father''s legacy.',
+       'ACTION', 'Jon Favreau', 5);
+
+INSERT INTO film(title, length, summary, genre, director, rating)
+VALUES('Iron Man 3', 130, 'When Tony Stark''s world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.',
+       'ACTION', 'Shane Black', 5);
+
+
+INSERT INTO actor(first_name, last_name, gender, bio)
+VALUES('Robert', 'Downey Jr', 'MALE',
+       'Robert Downey Jr. has evolved into one of the most respected actors in Hollywood. With an amazing list of credits to his name, he has managed to stay new and
+        fresh even after over four decades in the business. Downey was born April 4, 1965 in Manhattan, New York, the son of writer, director and filmographer Robert
+        Downey Sr. and actress Elsie Downey.')
